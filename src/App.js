75280a5044adccd56c97833ce3b678c700dbddcd
marndt93 <mattarndt14@gmail.com>
2017-11-08 14:10:15 -0500

    working horizontal component

--- src/App.js
+++ src/App.js
@@ -7,19 +7,30 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      active: false
+      active: false,
+      active2: false,
+      horizontal: true,
     }
   }
   update(e) {
-    this.setState({active: !this.state.active})
+    if (e.target.id === 'component_1'){
+      this.setState({active: !this.state.active})  
+    }
+    if (e.target.id === 'component_2'){
+      this.setState({active2: !this.state.active2})
+    }
+    if (e.target.id === 'toggle_orientation'){
+      this.setState({horizontal: !this.state.horizontal})
+    }
   }
   render() {
     return (
       <div className="App">
-        <button onClick={this.update.bind(this)}>Toggle Component 1</button>
-        <button onClick={this.update.bind(this)}>Toggle Component 2</button>  
+        <button id='component_1' onClick={this.update.bind(this)}>Toggle Component 1</button>
+        <button id='component_2' onClick={this.update.bind(this)}>Toggle Component 2</button>
+        <button id='toggle_orientation' onClick={this.update.bind(this)}>Toggle Orientation</button> 
         <div>{this.state.active}</div>      
-        <Resizing active = {this.state.active}>
+        <Resizing active = {this.state.active} active2 = {this.state.active2} horizontal = {this.state.horizontal}>
           <div>hello</div>
           <div>world</div>
         </Resizing>
--- src/Resizing.js
+++ src/Resizing.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './Resizing.css';
+
 
 class Resizing extends Component {
   render() {
+    let active
+    let active2
+    let orientation
+    if (this.props.active && this.props.active2){
+        active = '-half'
+        active2 = '-half'
+    }
+    else if (this.props.active && !this.props.active2){
+        active = '-full'
+        active2 = ''
+    }
+    else if (this.props.active2 && !this.props.active){
+        active2 = '-full'
+        active = ''
+    }
+    else if (!this.props.active && !this.props.active2){
+        active = ''
+        active2 = ''
+    }
+    if (this.props.horizontal){
+        orientation = '-horizontal'
+    }
+    else if (!this.props.horizontal){
+        orientation = '-vertical'
+    }
+    
     return (
-      <div className="App">
+      <div>
         {/*this.props.children*/}
-        <div style={{height: "50%;"}}>
+        <div id = '1' className={"Resize" + active + orientation}>
             {this.props.children[0]}
         </div>
-        <div style={{height: "50%;"}}>
+        <div id = '2' className={"Resize" + active2 + orientation}>
             {this.props.children[1]}
         </div>          
 
@@ -20,7 +48,8 @@ class Resizing extends Component {
 
 Resizing.PropTypes = {
     active: PropTypes.bool.isRequired,
+    active2: PropTypes.bool.isRequired,
     size: PropTypes.string,
-    orientation: PropTypes.string
+    horizontal: PropTypes.bool
 }
 export default Resizing;
